4238f99798198e7aebc2f18c907e1a47a5173225
Mukilarasu <mukilarasu2309@gmail.com>
2023-08-03 11:33:27 +0530

    JS code

---
+++ JS/sample.js
@@ -0,0 +1,35 @@
+
+var a = 120
+console.log("this is Var type:" + a)
+{
+    var a = 7
+    console.log("this is Var type :" + a)
+}
+console.log("this is Var type :" + a)
+
+
+let b = 120
+console.log("this is Var type:" + b)
+{
+    let b = 7
+    console.log("this is Var type :" + b)
+}
+console.log("this is Var type :" + b)
+
+
+const c = 120
+console.log("this is Var type:" + c)
+console.log("this is Var type :" + c)
+
+
+
+
+
+
+
+// let b = 120
+// console.log("this is let type:" + b)
+// const c = 120
+// console.log("this is const type:"+""+ c)
+
+
